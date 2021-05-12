c2d19d37873c5ae346c790b7a61724ae4d917ac3
imthor <48450247+imthor@users.noreply.github.com>
2021-05-12 10:39:02 +0530

    Update the title to 'n0ob | Tech'

--- _site/assets/js/lunrsearchengine.js
+++ _site/assets/js/lunrsearchengine.js
@@ -1,34 +1,34 @@
 
 var documents = [{
     "id": 0,
-    "url": "http://0.0.0.0:4000/noob/404.html",
+    "url": "http://0.0.0.0:4000/404.html",
     "title": "404",
     "body": "404Page does not exist!Please use the search bar at the top or visit our homepage! "
     }, {
     "id": 1,
-    "url": "http://0.0.0.0:4000/noob/about",
+    "url": "http://0.0.0.0:4000/about",
     "title": "About",
     "body": "Will be updated soon. "
     }, {
     "id": 2,
-    "url": "http://0.0.0.0:4000/noob/categories",
+    "url": "http://0.0.0.0:4000/categories",
     "title": "Categories",
     "body": ""
     }, {
     "id": 3,
-    "url": "http://0.0.0.0:4000/noob/",
+    "url": "http://0.0.0.0:4000/",
     "title": "Home",
     "body": "      Featured:                                                                                                                                                                                                           Hello World!                          :               Hello there. Welcome to n0ob!:                                                                                                                                                                       imthor                                11 May 2021                                                                                                                      All Stories:                                                                                                     Hello World!      :       Hello there. Welcome to n0ob!:                                                                                       imthor                11 May 2021                                            "
     }, {
     "id": 4,
-    "url": "http://0.0.0.0:4000/noob/robots.txt",
+    "url": "http://0.0.0.0:4000/robots.txt",
     "title": "",
     "body": "      Sitemap: {{ “sitemap. xml”   absolute_url }}   "
     }, {
     "id": 5,
-    "url": "http://0.0.0.0:4000/noob/hello-world/",
+    "url": "http://0.0.0.0:4000/hello-world/",
     "title": "Hello World!",
-    "body": "2021/05/11 - Hello there. Welcome to n0ob! Why n0ob?: Like most of the Engineers I know, I have been very bad when it comes to documentation. There is nothing that I procastinate more than writing documentation. I have been someone who tries a lot of different things during my free time which I never got to type down details of. Now that I look back, I feel that it would have been helpful for other people like me and even for my personal reference. So, this is my first ever conscious effort trying not to be lazy and note down things I try. Why now?: Well, with this never ending covid around, like most of you I’ve also been stuck home as well. Having no way to do anything outdoor and been frustrated for over more than a year, I’ve now been channeling my energy on building a home lab during free time. So, as part of that I’ve been working on lot of different small setups that I’m happy share for other n0obs who might find it helpful. Hope that you find the articles I’ll be sharing here useful and fun to read. See you around! "
+    "body": "2021/05/11 - Hello there. Welcome to n0ob! Why n0ob?: Like most of the Engineers I know, I have been very bad when it comes to documentation. There is nothing that I procastinate more than writing documentation. I have been someone who tries a lot of different things during my free time which I never got to type down details of. Now that I look back, I feel that it would have been helpful for other people like me and even for my personal reference. So, this is my first ever conscious effort trying not to be lazy and note down things I try. Why now?: Well, with this never ending covid around, like most of you I’ve also been stuck home as well. Having no way to do anything outdoor and been frustrated for over more than a year, I’ve now been channeling my energy on building a home lab during free time. So, as part of that I’ve been working on lot of different small setups that I’m happy to share for other n0obs who might find it helpful. Hope that you find the articles I’ll be sharing here useful and fun to read. See you around! "
     }];
 
 var idx = lunr(function () {
